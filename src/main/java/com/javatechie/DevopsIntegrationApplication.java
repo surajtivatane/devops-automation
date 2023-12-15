f80c2df2483e3ee8c2a0767f1b23d7211e553d39
surajtivatane <104413339+surajtivatane@users.noreply.github.com>
2023-12-15 10:23:47 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to globant originations";
+		return "welcome to globant origination";
 	}
 
 	public static void main(String[] args) {
